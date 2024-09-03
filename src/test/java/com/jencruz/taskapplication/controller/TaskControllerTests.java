e36d4ba8db5a63f1a62188c31343456ef9c428a8
jencruz21 <maxcruz4455@gmail.com>
2024-09-03 20:09:32 +0800

    chore: change properties file to yaml

--- src/test/java/com/jencruz/taskapplication/controller/TaskControllerTests.java
+++ src/test/java/com/jencruz/taskapplication/controller/TaskControllerTests.java
@@ -3,8 +3,6 @@ package com.jencruz.taskapplication.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jencruz.taskapplication.service.TaskService;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
